0f6eec4308ffc89675e6ecb8e2a60a0e20f38e67
Dustin Speer <dspeer@outlook.com>
2022-11-08 09:04:30 -0500

    Services and auth

--- Program.cs
+++ Program.cs
@@ -1,82 +1,137 @@
 using Tracker.Data;
 using Microsoft.EntityFrameworkCore;
 using Tracker.Models;
+using Tracker.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-//builder.Services.AddDbContext<TrackerContext>(options => options.UseSqlite("Data Source=Tracker.db"));
 builder.Services.AddSqlite<TrackerContext>("Data Source=Tracker.db");
-
 builder.Services.AddTransient<DbInitializer>();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<ICheckinService, CheckinService>();
+builder.Services.AddScoped<ICheckpointService, CheckpointService>();
+builder.Services.AddScoped<IMessageService, MessageService>();
+builder.Services.AddScoped<IMonitorService, MonitorService>();
+builder.Services.AddScoped<IParticipantService, ParticipantService>();
+builder.Services.AddScoped<IRaceService, RaceService>();
+builder.Services.AddScoped<ISegmentService, SegmentService>();
+builder.Services.AddScoped<IWatcherService, WatcherService>();
+builder.Services.AddScoped<ILeaderService, LeaderService>();
+builder.Services.AddScoped<IAlertMessageService, AlertMessageService>();
+builder.Services.AddSingleton<SlackService>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 string Auth0Domain = builder.Configuration["Auth0Config:Domain"];
-string Auth0ClientId = builder.Configuration["Auth0Config:AppClientId"];
-string Auth0ClientSecret = builder.Configuration["Auth0Config:AppClientSecret"];
 string Auth0Audience = builder.Configuration["Auth0Config:Audience"];
+string Auth0ClientId = builder.Configuration["Auth0Config:ClientId"];
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = Auth0Domain;
+        options.Audience = Auth0Audience;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidAudience = Auth0Audience,
+            ValidIssuer = Auth0Domain,
+            NameClaimType = ClaimTypes.NameIdentifier
+        };
+    });
+
+builder.Services
+    .AddAuthorization(options =>
+    {
+        options.AddPolicy("admin", policy => policy.RequireAuthenticatedUser().RequireClaim("scope", "admin"));
+    });
+
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Tracker API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/seed", (DbInitializer initializer) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    initializer.Initialize();
+}).RequireAuthorization();
 
-app.MapGet("/seed", (DbInitializer initializer) => 
+app.MapGet("/races", async (IRaceService raceService) =>
 {
-    //initializer.Initialize();
+    return await raceService.GetRacesAsync();
 });
 
-app.MapGet("/races", async (TrackerContext db) =>
+app.MapGet("/races/{raceId}", async (Guid raceId, IRaceService raceService) =>
 {
-    return await db.Races.ToListAsync();
+    return await raceService.GetRaceAsync(raceId);
+}).RequireAuthorization();
+
+app.MapGet("/segments", async (ISegmentService segmentService) =>
+{
+    return await segmentService.GetSegmentsAsync();
 });
 
-app.MapGet("/segments", async (TrackerContext db) =>
+app.MapGet("/segments/{segmentId}", async (Guid segmentId, ISegmentService segmentService) =>
 {
-    return await db.Segments.ToListAsync();
+    return await segmentService.GetSegmentAsync(segmentId);
 });
 
-app.MapGet("/checkpoints", async (TrackerContext db) =>
+app.MapGet("/checkpoints", async (ICheckpointService checkpointService) =>
 {
-    return await db.Checkpoints.ToListAsync();
+    return await checkpointService.GetCheckpointsAsync();
 });
 
-app.MapGet("/participants", async (TrackerContext db) =>
+app.MapGet("/checkpoints/{checkpointId}", async (Guid checkpointId, ICheckpointService checkpointService) =>
 {
-    return await db.Participants.ToListAsync();
+    return await checkpointService.GetCheckpointAsync(checkpointId);
 });
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/participants", async (IParticipantService participantService) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    return await participantService.GetParticipantsAsync();
+});
+
+app.MapGet("/participants/{participantId}", async (Guid participantId, IParticipantService participantService) =>
+{
+    return await participantService.GetParticipantAsync(participantId);
+});
 
 app.Run();
-
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
